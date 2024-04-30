ed14d436d5af082d8c5093fb2b2b82ac865ed245
Patrik Kleindl <pkleindl@gmail.com>
2024-04-30 11:18:48 +0200

    fixed ldap start

--- bootstrap/deploy.sh
+++ bootstrap/deploy.sh
@@ -3,3 +3,6 @@ kubectl create namespace argocd
 kubectl create namespace kafka-confluent-poc
 kustomize build --enable-helm . | kubectl apply -f -
 kubectl apply -f argocd.yaml
+kubectl apply -f cfk.yaml
+kubectl apply -f ../dev/cfk-demo.yaml
+kubectl apply -f ../ldap/ldap.yaml
