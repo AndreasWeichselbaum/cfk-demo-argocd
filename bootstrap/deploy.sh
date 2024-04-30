187d2320703a148d08cf178c075d2173f16f7ec4
Patrik Kleindl <pkleindl@gmail.com>
2024-04-30 10:22:28 +0200

    added namespaces

--- bootstrap/deploy.sh
+++ bootstrap/deploy.sh
@@ -1,2 +1,5 @@
+kubectl create namespace confluent
+kubectl create namespace argocd
+kubectl create namespace kafka-confluent-poc
 kustomize build --enable-helm . | kubectl apply -f -
 kubectl apply -f argocd.yaml
