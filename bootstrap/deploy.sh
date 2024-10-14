4306d89b281a976d99c93965b27cb4a40bc9598a
Bernhard Berg <34011017+Colfenor@users.noreply.github.com>
2024-10-14 14:29:57 +0200

    add config repo as ArgoCD app

--- bootstrap/deploy.sh
+++ bootstrap/deploy.sh
@@ -5,4 +5,5 @@ kustomize build --enable-helm . | kubectl apply -f -
 kubectl apply -f argocd.yaml
 kubectl apply -f cfk.yaml
 kubectl apply -f ../dev/cfk-demo.yaml
+kubectl apply -f ../dev/cfk-demo-kafka-configuration.yaml
 kubectl apply -f ../ldap/ldap.yaml
