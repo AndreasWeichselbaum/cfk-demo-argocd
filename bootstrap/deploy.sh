957f71fce22fc012fad952307910110d95e808d0
Peter Haraszti <peter.haraszti@bearingpoint.com>
2026-01-21 17:20:57 +0100

    Configure MinIO for log archivation

--- bootstrap/deploy.sh
+++ bootstrap/deploy.sh
@@ -18,3 +18,18 @@ kubectl apply -f ../dev/aw-rbac/example-role.yaml
 kubectl apply -f ../dev/aw-rbac/example-sa.yaml
 kubectl apply -f ../dev/aw-rbac/example-clusterrole.yaml
 kubectl apply -f ../dev/aw-rbac/example-crb.yaml
+
+
+helm repo add minio https://charts.min.io/
+helm repo update
+
+helm install argo-artifacts minio/minio \
+  --set fullnameOverride=argo-artifacts \
+  --set mode=standalone \
+  --set service.type=LoadBalancer
+
+kubectl port-forward pod/argo-artifacts-<POD-ID> 9001:9001
+
+Decode the argo-artifacts secret with k9s, log in to minio UI at localhost:9001
+
+Create a bucket named "my-bucket"
